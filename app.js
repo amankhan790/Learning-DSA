1004158a6121f69753f38607031857f4c6b21a60
amankhan790 <amankassar790@gmail.com>
2026-01-03 23:02:57 +0530

    mirror right triangle

--- app.js
+++ app.js
@@ -11,26 +11,46 @@ let prompt = require("prompt-sync")();
 //   console.log();
 // }
 
-// pattern two
+// left angel triangle
 // let n = Number(prompt("Enter a number :"));
-// // let m = Number(prompt("Enter a number :"));
 
-// // for (let i = n; i =1; i++) {
-// //   for (let j = n; j <=i; j++) {
-// //     process.stdout.write("*");
-// //   }
-// //   console.log();
-// // }
+// for (let i = 1; i <= n; i++) {
+//   for (let j = n; j <= i; j++) {
+//     process.stdout.write("*");
+//   }
+//   console.log();
+// }
 
-// printing english letter
+// right angel triangle
+// let n = Number(prompt("Enter a number :"));
+// for (let i = 1; i <= n; i++) {
+//   for (let j = n; j >= i; j--) {
+//     process.stdout.write("*");
+//   }
+//   console.log();
+// }
+
+// Mirror right angel triangle
 let n = Number(prompt("Enter a number :"));
-
 for (let i = 1; i <= n; i++) {
-  let ascii = 65;
+  for (let j = 1; j <= n - i; j++) {
+    process.stdout.write("  ");
+  }
+
   for (let j = 1; j <= i; j++) {
-    process.stdout.write(String.fromCharCode(ascii) + " ");
-    ascii++;
+    process.stdout.write("* ");
   }
   console.log();
-  
 }
+
+// printing english letter
+// let n = Number(prompt("Enter a number :"));
+
+// for (let i = 1; i <= n; i++) {
+//   let ascii = 65;
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write(String.fromCharCode(ascii) + " ");
+//     ascii++;
+//   }
+//   console.log();
+// }
