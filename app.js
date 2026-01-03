f0c086af8fdc05882a049949b3c357db84bd6612
amankhan790 <amankassar790@gmail.com>
2026-01-03 21:47:34 +0530

    printing english letter

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 let prompt = require("prompt-sync")();
 
-let n = Number(prompt("Enter a number :"));
-let m = Number(prompt("Enter a number :"));
+// pattern one
+// let n = Number(prompt("Enter a number :"));
+// let m = Number(prompt("Enter a number :"));
 
-for (let i = 0; i < m; i++) {
-  for (let j = 0; j < n; j++) {
-    process.stdout.write("*");
+// for (let i = 0; i < m; i++) {
+//   for (let j = 0; j < n; j++) {
+//     process.stdout.write("*");
+//   }
+//   console.log();
+// }
+
+// pattern two
+// let n = Number(prompt("Enter a number :"));
+// // let m = Number(prompt("Enter a number :"));
+
+// // for (let i = n; i =1; i++) {
+// //   for (let j = n; j <=i; j++) {
+// //     process.stdout.write("*");
+// //   }
+// //   console.log();
+// // }
+
+// printing english letter
+let n = Number(prompt("Enter a number :"));
+
+for (let i = 1; i <= n; i++) {
+  let ascii = 65;
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write(String.fromCharCode(ascii) + " ");
+    ascii++;
   }
   console.log();
+  
 }
