3df6a6b397b7b5740a2645ee585edf0c2d658acc
amankhan790 <amankassar790@gmail.com>
2026-01-11 15:14:58 +0530

    moving all o's to left and all 1's to right

--- arrays.js
+++ arrays.js
@@ -53,3 +53,19 @@
 // console.log(arr);
 
 
+
+// move all o's to left and all 1's to right
+let arr = [1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0];
+
+let i = 0,
+  j = 0;
+while (i<arr.length) {
+  if (arr[i] == 0) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    j++;
+  }
+  i++;
+}
+console.log(arr);
