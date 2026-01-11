693cf7110c3c9a100c6e9880ea126b69a0aa348a
amankhan790 <amankassar790@gmail.com>
2026-01-11 15:14:20 +0530

    reversing array without extra space

--- arrays.js
+++ arrays.js
@@ -25,15 +25,31 @@
 // }
 // console.log(sMax, max);
 
-// Reversing the array
-let arr = [20, 40, 99, 38, 206, 384];
-let temp = new Array(arr.length)
+// // Reversing the array
+// let arr = [20, 40, 99, 38, 206, 384];
+// let temp = new Array(arr.length)
 
-let j = 0;
-for(let i = arr.length - 1; i>=0; i--){
-  temp[j] = arr[i]
-  j++
-}
-console.log(temp);
+// let j = 0;
+// for(let i = arr.length - 1; i>=0; i--){
+//   temp[j] = arr[i]
+//   j++
+// }
+// console.log(temp);
+
+
+// Reversing the array without extra space with while loop
+// let arr = [10, 20, 30, 40, 50];
+
+// let i = 0, j = arr.length - 1;
+
+// while (i != j) {
+//   let temp = arr[i];
+//   arr[i] = arr[j];
+//   arr[j] = temp;
+//   i++;
+//   j--;
+// }
+
+// console.log(arr);
 
 
